dd1dd979372ec2f066b3c7fbf3c5a2908115b607
loicy <loicy@Loic-Yondjeu.localdomain>
2017-11-12 04:00:19 +0100

    fail test

--- Bank/src/de/bank/konto/KontoTest.java
+++ Bank/src/de/bank/konto/KontoTest.java
@@ -5,7 +5,7 @@ import junit.framework.TestCase;
 public class KontoTest extends TestCase {
 	
 	public void testAccountCreation(){
-		Konto testAccount = new Konto("Loic", "1234");
+		Konto testAccount = new Konto("Loic", "geheim");
 		testAccount.einzahlen(1000.0);
 		assertEquals(1000.0, testAccount.getKontostand());
 	}
